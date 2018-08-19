a3531309a02c5782af6a88e30ecd0bd5ee8d059b
Hyeong Joon Suh <hjsuh18@gmail.com>
2018-08-19 23:59:09 +0900

    Added button javascript properties
    Buttons work as needed, with automatic tab closing. The part
    missing is the visual coutdown.

--- background.js
+++ background.js
@@ -1,15 +1,74 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// background script will probably be used for detecting the shortcut
-
-
-'use strict';
-
+// Initialization when extension is installed
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
+	// bombs is an object storing tabId and tiemoutId of bomb activated tabs
+	chrome.storage.sync.set({"bombs": {}});
+
+	// store the default start time for time bomb
+	chrome.storage.sync.set({"time": {"hour": "01", "minute": "00", "second":"00"}});
 });
 
+
+// receive message from content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	var message = request.message;
+	var tabId = request.tabId;
+	var timeoutId = request.timeoutId;
+	var time = request.time;
+
+	// set a time to close the tab
+	if (message === "closeTab"){
+		console.log("Close tab");
+
+		timeoutId = setTimeout(function(){
+			// close the tab when countdown is done
+			chrome.tabs.remove(tabId);
+
+			// remove tab from bombs, since bomb closed tab
+			chrome.storage.sync.get("bombs", function(items){
+				var map = items["bombs"];
+				delete map[tabId];
+				update_bombs(map);
+			});
+		}, time);
+
+		// get activated bombs object from storage
+		chrome.storage.sync.get("bombs", function(items){
+			var map = items["bombs"];
+
+			// add new pair for new TabBomb tab and update stored variable
+			map[tabId] = timeoutId;
+			update_bombs(map);
+		});
+	}
+	// clear the timer set for closing a tab
+	else if (request.message === "resetTab"){
+		console.log("Reset Tab")
+		// use tabid to find timeoutid
+		chrome.storage.sync.get("bombs", function(items){
+			var map = items["bombs"];
+
+			// deactivate bomb on tab
+			timeoutId = map[tabId];
+			clearTimeout(timeoutId);
+
+			// remove tab from bombs, since bomb has been reset
+			delete map[tabId];
+			update_bombs(map);
+		});
+	}
+});
+
+// print object to visualize for debugging purposes
+function print_map(map){
+	console.log("Key-Value pairs of map");
+	for (key in map){
+		console.log("key: " + key + " value: " + map[key]);
+	}
+}
+
+function update_bombs(bombs){
+	chrome.storage.sync.set({"bombs": bombs}, function(){
+		console.log("Bombs successfully updated");
+		print_map(bombs);
+	});
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,72 @@
-$(function(){
-	$(#start_timer).click(function(){
-		console.log("Start timer button has been clicked");
+function initialize_timer(){
+	chrome.storage.sync.get("time", function(items){
+		var time = items["time"];
+		$('#hour').empty();
+		$("#hour").append(time["hour"]);
+		$('#minute').empty();
+		$("#minute").append(time["minute"]);
+		$('#second').empty();
+		$("#second").append(time["second"]);
 	});
-	$(#stop_timer).click(function(){
-		console.log("Stop time button has been clicked");
+}
+
+// initialize timer to saved values when loading popup
+initialize_timer();
+
+$(function(){
+	$('#startTimer').click(function(){
+		// get current tab id and remove it after a certain amount of time
+		chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs){
+			tabId = tabs[0].id;
+			chrome.storage.sync.get("time", function(items){
+				var time = items["time"];
+				time  = convert_to_milliseconds(parseInt(time["hour"]), parseInt(time["minute"]), parseInt(time["second"]));
+				console.log(time);
+				chrome.runtime.sendMessage({"tabId": tabId, "message": "closeTab", "time": time})
+			});
+		});
+
+		// Start the timer for tab
+	});
+	$('#resetTimer').click(function(){
+		// reset the timer for tab
+		chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs){
+			tabId = tabs[0].id;
+			chrome.runtime.sendMessage({"tabId": tabId, "message": "resetTab"})
+		});
+		initialize_timer();
+	});
+
+	$('#timerDefault').submit(function(e){
+		var hour = $("#hourSelect").val();
+		var minute = $("#minuteSelect").val();
+		var second = $("#secondSelect").val();
+
+		chrome.storage.sync.get("time", function(items){
+			var time = items["time"];
+			if (hour != null)
+				time["hour"] = hour;
+			if (minute != null)
+				time["minute"] = minute;
+			if (second != null)
+				time["second"] = second;
+			chrome.storage.sync.set({"time": time}, function(){
+				console.log("Time has successfully been updated");
+			})
+		});
+
+		// if timer is not running, update
+		chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs){
+			tabId = tabs[0].id;
+			chrome.storage.sync.get("bombs", function(items){
+				var map = items["bombs"];
+				if (!(tabId in map)){
+					initialize_timer();
+				}
+			});
+		});
+
+		e.preventDefault();
 	});
 });
 
@@ -11,6 +74,7 @@ $(function(){
 function countdown(){
 	var now = new Date();
 
+	console.log(now);
 	// get time when tab needs to be closed from storage
 
 	// calculate the time left given two pieces of information
@@ -19,7 +83,7 @@ function countdown(){
 
 	// after 1000 milliseconds, call the countdown function
 	// this creates an infinite loop, updating the time left every second
-	setTimeout(countdown, 1000)
+	setInterval(countdown, 1000);
 }
 
 
@@ -28,4 +92,9 @@ function countdown(){
 function start_countdown(){
 	var now = new Date();
 
+}
+
+// calculate total number of milliseconds in hour, minute, second
+function convert_to_milliseconds(hour, minute, second){
+	return (hour * 3600 + minute * 60 + second) * 1000;
 }
